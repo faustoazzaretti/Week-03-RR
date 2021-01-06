146e20b1f8ac3d104271957913fd30f94661ce4a
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-01-06 15:44:06 -0300

    Add app.js and obtain elements in window onload

---
+++ app.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    //obtain the inputs
+    inputName = document.getElementById('name');
+    intputEmail = document.getElementById('email');
+    inputPassword = document.getElementById('password');
+    inputRepeatPassoword = document.getElementById('repeatPassword');
+    inputAge = document.getElementById('age');
+    inputPhone = document.getElementById('phone');
+    inputAdress = document.getElementById('adress');
+    inputCity = document.getElementById('city');
+    inputCp = document.getElementById('cp');
+    inputDNI = document.getElementById('dni');
+}
