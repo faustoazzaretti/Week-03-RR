e27fd6ef5d992fb7226e96df21fc5f86bbc39782
Fausto Azzaretti <faustoazzaretti95@gmail.com>
2021-01-11 09:44:49 -0300

    Add link ref to Home and Send buttons

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+validateName = function () {
+    if (inputName.value < 7) {
+        alert('El nombre completo debe tener al menos 6 letras.')
+    }
+}
+
+validateEmail = function () {
+    if (!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor))) {
+        console.log('Debe ingresar un email correcto');
+    }
+}
+
 window.onload = function () {
     //obtain the inputs
     inputName = document.getElementById('name');
     intputEmail = document.getElementById('email');
+    valor = document.getElementById('email').value;
     inputPassword = document.getElementById('password');
     inputRepeatPassoword = document.getElementById('repeatPassword');
     inputAge = document.getElementById('age');
@@ -10,4 +23,11 @@ window.onload = function () {
     inputCity = document.getElementById('city');
     inputCp = document.getElementById('cp');
     inputDNI = document.getElementById('dni');
+    //obtain the button
+    sendButton = document.getElementById('sendButton');
+    sendButton.addEventListener('click', validateName);
+
+    //listeners 
+    inputName.addEventListener("blur", validateName);
+    intputEmail.addEventListener("blur", validateEmail);
 }
